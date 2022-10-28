44429c70b39f987ee170132395031635f4bd4e40
andrey_kharchenko <KharchenkoAndru@gmail.com>
2022-10-28 08:48:57 +0300

    #1 add themes

---
+++ src/themes/AppThemeProvider.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+
+const theme = createTheme({
+    typography: {
+        allVariants: {
+            textTransform: 'none',
+            fontSize: 15
+        }
+    },
+    palette: {
+        primary: {
+            main: '#3853D8'
+        },
+        secondary: {
+            main: grey[700]
+        }
+    },
+    components: {
+        MuiTypography: {
+            defaultProps: {
+                sx: {
+                    px: 1
+                },
+                variant: 'subtitle2',
+                textTransform: 'capitalize'
+            }
+        },
+        MuiStack: {
+            defaultProps: {
+                sx: {
+                    px: 2,
+                    py: 1
+                }, 
+                spacing: 2,
+                direction: 'row',
+            }
+        },
+        MuiPaper: {
+            defaultProps: {
+                elevation: 0,
+            }
+        },
+        MuiLink: {
+            defaultProps: {
+                sx: {
+                    color: theme => theme.palette.secondary.main
+                },
+                underline: 'none'
+            }
+        },
+        MuiButton: {
+            defaultProps: {
+                size: 'small',
+                disableRipple: true
+            },
+        },
+        MuiTab: {
+            defaultProps: {
+                disableRipple: true
+            }
+        }
+
+    }
+});
+
+const AppThemeProvider = (props: any) => {
+    return <ThemeProvider theme={theme}> {props.children} </ThemeProvider>
+}
+
+export default AppThemeProvider;
---
+++ src/themes/commonStyles.ts
@@ -0,0 +1,49 @@
+/* Flexbox styles */
+
+export const flexBetween = {
+    display: 'flex',
+    justifyContent: 'space-between'
+};
+
+export const flexBetweenCenter = {
+    display: 'flex',
+    justifyContent: {xs: 'center', md: 'space-between'},
+    alignItems: 'center'
+};
+
+export const footerLayout = {
+    display: 'flex',
+    flexDirection: {xs: 'column'},
+    justifyContent: {xs: 'center', md: 'space-between'},
+    alignItems: 'center'
+};
+
+export const flexCenter = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
+export const fullWidthFlex = {
+    display: 'flex',
+    width: '100%'
+};
+
+export const justifyCenter = {display: 'flex', justifyContent: 'center'};
+
+export const dFlex = {
+    display: 'flex',
+    flexDirection: 'row'
+}
+
+export const fixedBottom = {
+   position: 'absolute', 
+   bottom: 100,
+   width: '100%'
+}
+
+export const defaultButton = {
+    fontWeight: '800',
+    textTransform: 'uppercase'
+}
+
